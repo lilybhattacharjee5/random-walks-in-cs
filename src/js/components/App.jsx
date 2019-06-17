a4044c4d5626f2c6901354b4fdb5c0bca854a872
Lily Bhattacharjee <lily.bhattacharjee@LilyBhaarjeeMBP.attlocal.net>
2019-06-16 21:16:53 -0700

    added basic directions, should update later

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -5,6 +5,7 @@ import ConnectedDWGraph from './DoctorWhoGraph';
 const App = () => (
 	<div className = "container">
 		<h1>Interactive <i>Doctor Who</i></h1>
+		<p><b>Directions</b>: Please select either a doctor OR a season and episode number to view a representative interaction graph between <i>Doctor Who</i> characters. The gradient from red to blue edge colors represents involvement ranging from negative sentiment dialogue to positive sentiment dialogue respectively.</p>
 		<br />
 		<DoctorWhoForm />
 		<br />
