c42f0a9c4962cf9341c288cfd4ef320e2b0fcc27
Lily Bhattacharjee <lily.bhattacharjee@LilyBhaarjeeMBP.attlocal.net>
2019-06-16 21:48:25 -0700

    moved and renamed build folder

---
+++ public/precache-manifest.1ed6d78cc70fac5dd9060e16f7ad0dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d8227cfba79092761fb0611521126c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a26514ef777adbeb35d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f1682f207415a4e4f370",
+    "url": "/static/js/2.cc6a93d7.chunk.js"
+  },
+  {
+    "revision": "2a26514ef777adbeb35d",
+    "url": "/static/js/main.88f6f3af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
